a1b7835e5b39e7ed04ff0c989b12277f3b795a84
Hwisung <heesong1130@gmail.com>
2021-05-11 20:55:35 +0900

    Remove: Detail브랜치의 filter, gift폴더 삭제

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import Detail from '../../Components/Detail/Detail';
 
 class Main extends React.Component {
   render() {
-    return <div>haha</div>;
+    return <Detail />;
   }
 }
 
--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './Pages/Main/Main';
-import Detail from './Components/Detail/Detail';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Detail} />
+          <Route exact path="/" component={Main} />
         </Switch>
       </Router>
     );
