d774689e359641f3999b3ed4de1d18c542d982f0
Hwisung <heesong1130@gmail.com>
2021-05-11 20:47:49 +0900

    Remove:

--- src/Routes.js
+++ src/Routes.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Main from './Pages/Main/Main';
-import Gift from './Components/Gift/Gift';
 import Detail from './Components/Detail/Detail';
-import FilterBar from './Components/FilterBar/FilterBar';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/Detail" component={Detail} />
-          <Route exact path="/Gift" component={Gift} />
-          <Route exact path="/FilterBar" component={FilterBar} />
+          <Route exact path="/" component={Detail} />
         </Switch>
       </Router>
     );
