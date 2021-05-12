d19ed99ddb92d19b2901ce88f42abe2d34ca5650
Hwisung <heesong1130@gmail.com>
2021-05-12 09:57:35 +0900

    Modify:레이아웃 수정중

--- src/Components/GiftList/Gift/Gift.js
+++ src/Components/GiftList/Gift/Gift.js
@@ -19,7 +19,32 @@ class Gift extends React.Component {
     const { gift } = this.state;
     return (
       <div className="Gift">
-        <div>{gift.id}</div>
+        <div className="giftBox">
+          <div className="giftModel">
+            <div className="giftImg">
+              <img alt="상품사진" src={gift.img}></img>
+            </div>
+            <div className="giftNameSize">
+              <p>{gift.productName}</p>
+              <p>
+                ₩ {gift.price} / {gift.size}
+              </p>
+            </div>
+          </div>
+          <div className="giftInfo">
+            <ul>
+              <li>
+                <p>기프트 소개</p>
+                <p>{gift.desc}</p>
+              </li>
+              <li>
+                <p>구성품</p>
+                <p>{gift.contents}</p>
+              </li>
+            </ul>
+          </div>
+          <button className="addCart">카트에 추가하기 - ₩ ${gift.price}</button>
+        </div>
       </div>
     );
   }
