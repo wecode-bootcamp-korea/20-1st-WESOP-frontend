40d3db7ac5bce397e9ef707889ad3b7c19c01fe7
dosilv <do.silver2@gmail.com>
2021-05-12 11:56:22 +0900

    Add: Main Menu 레이아웃 및 애니메이션 구현

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -4,40 +4,120 @@ import './MainMenu.scss';
 class MainMenu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isHover: { li0: false, li1: false, li2: false } };
+    this.state = {
+      wheel: 0,
+      firstRequest: '제품보기',
+      secondRequest: '',
+    };
   }
 
+  componentDidMount() {
+    window.addEventListener('wheel', this.handleWheel);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('wheel', this.handleWheel);
+  }
+
+  handleWheel = e => {
+    this.setState({ wheel: this.state.wheel + e.deltaY * 0.001 });
+  };
+
+  handleFirstRequest = menu => {
+    this.setState(
+      {
+        firstRequest: '',
+        secondRequest: '',
+      },
+      () => {
+        this.setState({ firstRequest: menu });
+      }
+    );
+  };
+
+  handleSecondRequest = category => {
+    this.setState(
+      {
+        secondRequest: [],
+      },
+      () => {
+        this.setState({
+          secondRequest: category,
+        });
+      }
+    );
+  };
+
   render() {
-    const { isHover } = this.state;
+    const { wheel, firstRequest, secondRequest } = this.state;
+    const { handleFirstRequest, handleSecondRequest } = this;
 
     return (
       <div className="MainMenu">
         <div className="menuNav">
           <ul>
-            {UPPERCATEGORIES.map((title, index) => (
+            {Object.keys(CATEGORIES).map(menu => (
               <li
                 onMouseOver={() => {
-                  // const newHover = { ...ALLNOTHOVER };
-                  // newHover[`li${index}`] = true;
-                  this.setState({
-                    isHover: { ...ALLNOTHOVER, [`li${index}`]: true },
-                  });
+                  handleFirstRequest(menu);
                 }}
               >
-                {title}
-                <hr style={{ width: isHover['li' + index] && `100%` }} />
+                {menu}
+                <hr style={{ width: firstRequest === menu && `100%` }} />
               </li>
             ))}
           </ul>
           <i class="fas fa-times" />
         </div>
-        <img alt="wesop logo" src="images/wesop.png" className="logo" />
-        <div className="category">
-          {CATEGORIES['제품보기'].map((cate, index) => {
-            return (
-              <li style={{ animationDelay: `${0.1 * index}s` }}>{cate}</li>
-            );
-          })}
+        <div className="column">
+          <div className={`firstMenu ${secondRequest && `nextMenu`}`}>
+            <img
+              alt="wesop logo"
+              src="images/wesop.png"
+              className="logo"
+              style={{ transform: `rotate(${wheel}turn)` }}
+            />
+            <div className="category">
+              {firstRequest === '검색' && (
+                <>
+                  <i class="fas fa-arrow-right" />
+                  <input />
+                </>
+              )}
+              <ul>
+                {(firstRequest
+                  ? Object.keys(CATEGORIES[firstRequest])
+                  : []
+                ).map((category, index) => (
+                  <li
+                    style={{
+                      animationDelay: `${index * 0.1}s`,
+                      borderColor: category === secondRequest && '#333',
+                    }}
+                    onMouseOver={() => {
+                      handleSecondRequest(category);
+                    }}
+                  >
+                    {category}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {secondRequest && (
+            <div className="secondMenu">
+              <ul className="category">
+                {(Array.isArray(secondRequest)
+                  ? []
+                  : CATEGORIES[firstRequest][secondRequest]
+                ).map((category, index) => (
+                  <li style={{ animationDelay: `${index * 0.1 + 0.2}s` }}>
+                    {category}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     );
@@ -46,10 +126,40 @@ class MainMenu extends React.Component {
 
 export default MainMenu;
 
-const UPPERCATEGORIES = ['제품보기', '읽기', '검색'];
 const CATEGORIES = {
-  제품보기: ['스킨', '헤어', '바디 & 핸드', '향수', '기프트 가이드'],
-  읽기: [],
-  검색: [],
+  제품보기: {
+    스킨: [
+      '모두 보기',
+      '스킨 케어 기프트',
+      '클렌저',
+      '각질 제거',
+      '트리트먼트 & 마스크',
+      '토너',
+      '하이드레이터',
+      '립 & 아이',
+    ],
+    헤어: ['모두 보기', '샴푸', '컨디셔너', '트리트먼트', '그루밍'],
+    '바디 & 핸드': [
+      '핸드 및 바디 케어 기프트',
+      '핸드',
+      '바디',
+      '퍼스널 케어',
+      '번들',
+    ],
+    향수: [
+      '로즈 오 드 퍼퓸',
+      '로즈 앙상블',
+      '휠 오 드 퍼퓸',
+      '마라케시 인텐스 퍼퓸',
+    ],
+    '기프트 가이드': ['모든 기프트', '이솝에서 사랑받는 제품들'],
+  },
+
+  읽기: {
+    '더 아테네움': [],
+    '회사 소개': [],
+    철학: [],
+    'Taxonomy of Design': [],
+  },
+  검색: { 인기검색어: ['클렌저', '페뷸러스', '향수'] },
 };
-const ALLNOTHOVER = { li0: false, li1: false, li2: false };
