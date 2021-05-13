ef1d9eb859ba64a34024b203a72ecdf867a1c1ce
dosilv <do.silver2@gmail.com>
2021-05-13 17:33:21 +0900

    Add: 초기 세팅

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.scss';
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="Footer">
+        <div className="column"></div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
