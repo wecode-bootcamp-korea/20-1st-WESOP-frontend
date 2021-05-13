b798be7e097d19685852daaf8651550a23f9a126
dosilv <do.silver2@gmail.com>
2021-05-13 21:36:06 +0900

    임시저장

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,10 +9,55 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="Footer">
-        <div className="column"></div>
+        <div className="gridContainer">
+          <div className="box">
+            <input
+              type="text"
+              placeholder="이메일 주소"
+              className="enterEmail"
+            />
+            <p>
+              이솝 제품, 서비스, 스토어, 행사, 문화적 관심사 등 다양한 소식을
+              받아볼 수 있도록 구독하세요.
+            </p>
+            <label>
+              <input type="checkbox" name="over14" className="over14" />
+              <span></span>
+              본인의 만 14세 이상 여부를 확인해주세요.
+            </label>
+          </div>
+          <div className="box">
+            <div className="title">주문 및 지원</div>
+            <ul>
+              <li>문의하기</li>
+              <li>자주 묻는 질문</li>
+              <li>배송 및 반품</li>
+              <li>문의하기</li>
+            </ul>
+          </div>
+          <div className="box">C</div>
+          <div className="box">D</div>
+          <div className="box">E</div>
+          <div className="box">F</div>
+          <div className="box">G</div>
+          <div className="box">H</div>
+        </div>
+        <div className="banner"></div>
       </div>
     );
   }
 }
 
 export default Footer;
+
+const FOOTER_DATA = [
+  {
+    '주문 및 지원': [
+      '문의 하기',
+      '자주 듣는 질문',
+      '배송 및 반품',
+      '이용 약관',
+    ],
+  },
+  { 서비스: ['라이브', '간결함과 진정성'] },
+];
