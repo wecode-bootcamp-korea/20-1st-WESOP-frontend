13bc1fe77dea43015f6affc7e0ef89ac6052a3f2
dosilv <do.silver2@gmail.com>
2021-05-14 16:32:07 +0900

    3번째 메뉴 구현 중

--- src/Components/MainMenu/MainMenu.js
+++ src/Components/MainMenu/MainMenu.js
@@ -8,6 +8,7 @@ class MainMenu extends React.Component {
       wheel: 0,
       firstRequest: '제품보기',
       secondRequest: '',
+      thirdRequest: '',
       menus: [],
       categories: [],
     };
@@ -48,6 +49,7 @@ class MainMenu extends React.Component {
       {
         firstRequest: '',
         secondRequest: '',
+        thirdRequest: '',
       },
       () => {
         this.setState({ firstRequest: upperMenu });
@@ -68,82 +70,109 @@ class MainMenu extends React.Component {
     );
   };
 
+  handleThirdRequest = category => {
+    this.setState(
+      {
+        thirdRequest: [],
+      },
+      () => {
+        this.setState({ thirdRequest: category });
+      }
+    );
+  };
+
   render() {
-    const { wheel, firstRequest, secondRequest, menus, categories } =
-      this.state;
-    const { handleFirstRequest, handleSecondRequest } = this;
+    const {
+      wheel,
+      firstRequest,
+      secondRequest,
+      thirdRequest,
+      menus,
+      categories,
+    } = this.state;
+    const { handleFirstRequest, handleSecondRequest, handleThirdRequest } =
+      this;
 
     let upperMenus = {
-      제품보기: this.state.menus,
+      제품보기: menus,
       읽기: ['더 아테네움', '회사 소개', '철학', 'Taxonomy of Design'],
       검색: ['인기검색어', '클렌저', '페뷸러스', '향수'],
     };
 
     return (
       <div className="mainMenu">
-        <div className="menuNav">
-          <ul>
-            {Object.keys(upperMenus).map((upperMenu, index) => (
-              <li
-                key={index}
-                onMouseOver={() => {
-                  handleFirstRequest(upperMenu);
-                }}
-              >
-                {upperMenu}
-                <hr style={{ width: firstRequest === upperMenu && `100%` }} />
-              </li>
-            ))}
-          </ul>
-          <i class="fas fa-times" />
-        </div>
-        <div className="column">
-          <div className={`firstMenu ${secondRequest && `nextMenu`}`}>
-            <img
-              alt="wesop logo"
-              src="images/wesop.png"
-              className="logo"
-              style={{ transform: `rotate(${wheel}turn)` }}
-            />
-            <div className="category">
-              {firstRequest === '검색' && (
-                <>
-                  <i class="fas fa-arrow-right" />
-                  <input />
-                </>
-              )}
-              <ul>
-                {(upperMenus[firstRequest] || []).map((menu, index) => (
-                  <li
-                    style={{
-                      animationDelay: `${index * 0.1}s`,
-                      borderColor: menu === secondRequest && '#333',
-                    }}
-                    onMouseOver={() => {
-                      handleSecondRequest(menu);
-                    }}
-                  >
-                    {menu}
-                  </li>
-                ))}
-              </ul>
-            </div>
+        <div className={`firstMenu ${secondRequest && `nextMenu`}`}>
+          <div className="menuNav">
+            <ul>
+              {Object.keys(upperMenus).map((upperMenu, index) => (
+                <li
+                  key={index}
+                  onMouseOver={() => {
+                    handleFirstRequest(upperMenu);
+                  }}
+                >
+                  {upperMenu}
+                  <hr style={{ width: firstRequest === upperMenu && `100%` }} />
+                </li>
+              ))}
+            </ul>
+            <i className="fas fa-times" />
           </div>
-          {secondRequest && (
-            <div className="secondMenu">
-              <ul className="category">
-                {(categories[secondRequest] || []).map((category, index) => (
-                  <li
-                    key={index}
-                    style={{ animationDelay: `${index * 0.1 + 0.2}s` }}
-                  >
-                    {category}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
+          <img
+            alt="wesop logo"
+            src="images/wesop.png"
+            className="logo"
+            style={{ transform: `rotate(${wheel}turn)` }}
+          />
+          <div className="category">
+            {firstRequest === '검색' && (
+              <>
+                <i class="fas fa-arrow-right" />
+                <input />
+              </>
+            )}
+            <ul>
+              {(upperMenus[firstRequest] || []).map((menu, index) => (
+                <li
+                  style={{
+                    animationDelay: `${index * 0.1}s`,
+                    borderColor: menu === secondRequest && '#333',
+                  }}
+                  onMouseOver={() => {
+                    handleSecondRequest(menu);
+                  }}
+                >
+                  {menu}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
+        {secondRequest && (
+          <div className="secondMenu">
+            <ul className="category">
+              {(categories[secondRequest] || []).map((category, index) => (
+                <li
+                  key={index}
+                  style={{
+                    animationDelay: `${index * 0.1 + 0.2}s`,
+                    borderColor: category === thirdRequest && '#333',
+                  }}
+                  onMouseOver={() => {
+                    handleThirdRequest(category);
+                  }}
+                >
+                  {category}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+        {thirdRequest && (
+          <div className="thirdMenu">
+            <ul className="category"></ul>
+          </div>
+        )}
       </div>
     );
   }
