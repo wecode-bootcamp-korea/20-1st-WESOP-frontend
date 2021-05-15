1dd5ab723500d93d41697eae3b781bf5f9f4bc89
choiwk <dbk03053@gmail.com>
2021-05-15 18:34:49 +0900

    임시저장

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -2,12 +2,21 @@ import React from 'react';
 import SignUp from '../../Components/SignUp/SignUp';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signUp: true,
+    };
+  }
+
+  signUpToggle = () => {
+    setTimeout(() => {
+      this.setState({ login: false });
+    }, 200);
+  };
+
   render() {
-    return (
-      <div>
-        <SignUp />
-      </div>
-    );
+    return <div>{/* <SignUp this.state.signUp/> */}</div>;
   }
 }
 
