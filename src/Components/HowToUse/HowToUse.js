9cf9b758f4f616f5fbb7ad0c8b45d3d4ded462a5
choiwk <dbk03053@gmail.com>
2021-05-17 10:47:12 +0900

    임시저장

--- src/Components/HowToUse/HowToUse.js
+++ src/Components/HowToUse/HowToUse.js
@@ -12,8 +12,16 @@ class HowToUse extends React.Component {
         <section className="aboutProduct">
           <div className="leftProductImage">
             <figure>
-              <img src="." alt="" />
+              <img alt="leftProductImage" src="/images/leftProduct.png" />
             </figure>
+          </div>
+          <div className="rightPart">
+            <div className="rightProductInfo">
+              <header>
+                <span>사용법</span>
+                <h1>제품을 사용할 때마다 감사의 마음을 전합니다.</h1>
+              </header>
+            </div>
           </div>
         </section>
       </div>
