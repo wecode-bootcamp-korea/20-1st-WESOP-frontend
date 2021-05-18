18e0d914dea4a5589233b76ab4c7ca171056b5e3
Hwisung <heesong1130@gmail.com>
2021-05-19 03:30:36 +0900

    Modify : Route수정

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Detail from '../../Components/Detail/Detail';
 import Footer from '../../Components/Footer/Footer';
+import Nav from '../../Components/Nav/Nav';
 
 class ProductDetail extends React.Component {
   render() {
     return (
       <>
+        <Nav />
         <Detail />
         <Footer />
       </>
