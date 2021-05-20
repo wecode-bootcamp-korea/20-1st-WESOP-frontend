c3b67410dfd223932abe3895bae0a38877bafa41
dosilv <do.silver2@gmail.com>
2021-05-20 19:27:55 +0900

    오류수정

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Login from '../../Components/Login/Login';
 import Nav from '../../Components/Nav/Nav';
 import MainIllust from '../../Components/MainIllust/MainIllust';
 import MainProduct from '../../Components/MainProduct/MainProduct';
@@ -7,15 +6,9 @@ import ProductList from '../../Components/ProductList/ProductList';
 import Footer from '../../Components/Footer/Footer';
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return (
       <div className="main">
-        <Login />
         <Nav />
         <MainIllust />
         <ProductList />
