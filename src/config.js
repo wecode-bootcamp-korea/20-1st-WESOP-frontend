df997ad429eaf552daed7b6edfaeaa9b74d9be91
dosilv <do.silver2@gmail.com>
2021-05-20 16:34:48 +0900

    Config 작업 merge 이후에 하기로

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const IP1 = '';
-const IP2 = '';
-
-export const GET_MENU_API = `${IP1}/menu_id`;
-export const GET_CATEGORY_API = `${IP1}/category_id`;
-export const GET_DETAIL_API = `${IP1}/product_id`;
-export const GET_CART_API = `${IP2}/order/cart`;
