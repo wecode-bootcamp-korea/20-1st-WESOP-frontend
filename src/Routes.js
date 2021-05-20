d826391410e966290a668876cd9fe1408f0924cc
choiwk <dbk03053@gmail.com>
2021-05-20 19:13:09 +0900

    충돌해결_GiftPage수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import GiftProductList from './Pages/GiftProductList/GiftProductList';
+import GiftPage from './Pages/GiftProductList/GiftPage';
 import Main from './Pages/Main/Main';
-<<<<<<< HEAD
-=======
 import Products from './Pages/Products/Products';
->>>>>>> origin
 import ProductDetail from './Pages/ProductDetail/ProductDetail';
 
 class Routes extends React.Component {
@@ -14,13 +11,10 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Main} />
-<<<<<<< HEAD
-=======
           <Route exact path="/products" component={Products} />
           <Route exact path="/products/:id" component={Products} />
           <Route exact path="/giftpage" component={GiftPage} />
           <Route exact path="/giftpage/:id" component={GiftPage} />
->>>>>>> origin
           <Route exact path="/productdetail" component={ProductDetail} />
         </Switch>
       </Router>
