6b7f3cbb6bb6103d2836926a90dc0b15868de5a0
choiwk <dbk03053@gmail.com>
2021-05-20 12:40:29 +0900

    Main.js불필요한 기능 삭제_scss정리

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -4,19 +4,11 @@ import SignUp from '../../Components/SignUp/SignUp';
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      signUp: true,
-    };
+    this.state = {};
   }
 
-  signUpToggle = () => {
-    setTimeout(() => {
-      this.setState({ login: false });
-    }, 200);
-  };
-
   render() {
-    return this.state.signUp && <SignUp signUpToggle={this.signUpToggle} />;
+    return <SignUp />;
   }
 }
 
